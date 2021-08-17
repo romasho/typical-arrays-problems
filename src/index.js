256d64e521a9101fcbc31c4005b19345bbebccfb
unknown <sholomitkyR@yandex.by>
2021-08-17 12:12:45 +0300

    feat:add if...esle for empty array and undefinded

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 
 exports.min = function min (array) {
-  return (array.sort(function(a,b) {return a-b})[0])
+  if (array == undefined || array.length == 0) {return 0} else {
+  return (array.sort(function(a,b) {return a-b})[0])}
 }
 
 exports.max = function max (array) {
-  return (array.sort(function(a,b) {return b - a})[0])
+  if (array == undefined || array.length == 0) {return 0} else {
+  return (array.sort(function(a,b) {return b - a})[0])}
 }
 
 exports.avg = function avg (array) {
+  if (array == undefined || array.length == 0) {return 0} else {
   let sum = 0
   for (let i = 0; i <array.length; i++) {
     sum += array[i]
   }
  return  (sum / array.length)
-}
+}}
