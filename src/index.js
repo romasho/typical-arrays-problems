bfc6fb260fb0e8aa54bda93b4abaf27c8ff69098
unknown <sholomitkyR@yandex.by>
2021-08-17 11:51:54 +0300

    feat: create 3 main function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (array.sort(function(a,b) {return a-b})[0])
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array.sort(function(a,b) {return b - a})[0])
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0
+  for (let i = 0; i <array.length; i++) {
+    sum += array[i]
+  }
+ return  (sum / array.length)
 }
